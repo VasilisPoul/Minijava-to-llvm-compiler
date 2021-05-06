01dee71699704d56247ccf695dcd9647570eac10
vasilisp100 <vasilis.p.100@gmail.com>
2021-05-06 17:01:11 +0300

    VarClass: - added size in variable class in order to claculate offsets

--- visitor/VarClass.java
+++ visitor/VarClass.java
@@ -3,9 +3,17 @@ package visitor;
 public class VarClass {
     public String name;
     public String type;
+    public int size;
 
     public VarClass(String name, String type){
         this.name = name;
         this.type = type;
+        if (type == "boolean"){
+            this.size = 1;
+        }
+        else if (type == "int"){
+            this.size = 4;
+        }
+        else this.size = 8;
     }
 }
