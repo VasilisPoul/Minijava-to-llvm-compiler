31608a5ce62cfe171ef178c2ed9fe47b5fde6527
vasilisp100 <vasilis.p.100@gmail.com>
2021-05-06 20:36:35 +0300

    added one more test

---
+++ test-files/test99 copy.java
@@ -0,0 +1,29 @@
+class test99{
+    public static void main(String[] a){
+	System.out.println(new Test().start());
+    }
+}
+
+class Test {
+
+    Test test;
+    int i;
+    boolean ab;
+    public int start(){
+	test = new Test();
+	i = test.next(this);
+	
+	return 0;
+    }
+
+    public int next(Test t){
+
+	return 0;
+    }
+
+}
+
+class Test1 extends Test{
+    boolean cd;
+    boolean i;
+}
