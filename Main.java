5d216effdcf06e478e783f0ff93a0eba6643ca4b
vasilisp100 <vasilis.p.100@gmail.com>
2021-05-06 18:10:19 +0300

    Main fixed a typo

--- Main.java
+++ Main.java
@@ -25,10 +25,10 @@ public class Main {
                 System.err.println("Program parsed successfully.");
                 DeclVisitor eval = new DeclVisitor();
                 root.accept(eval, null);
-                System.err.println("Declarations Visitor finished succesfully.");
+                System.err.println("Declarations Visitor finished successfully.");
                 TypeVisitor type = new TypeVisitor(eval.classDeclarations);
                 root.accept(type, null);
-                System.err.println("Type Checking Visitor finished succesfully.");
+                System.err.println("Type Checking Visitor finished successfully.");
             }
             catch(ParseException ex){
                 System.out.println(ex.getMessage());
