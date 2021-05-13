7cd1054560a82799dfaeb4ed40319d7b973aec7e
vasilisp100 <vasilis.p.100@gmail.com>
2021-05-13 14:59:36 +0300

    reversed Add.java

--- test-files/Add.java
+++ test-files/Add.java
@@ -1,10 +1,5 @@
-class Test {
-	public static void main(String[] args) {}
-  }
-  
-  class A {
-	// Error : double declaration of symbol x inside argument list
-	public int foo(A x, int x) {
-	  return 5;
+class Add {
+	public static void main(String[] a){
+		System.out.println(12 + 21);
 	}
-  }
+}
